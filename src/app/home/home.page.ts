9359effe588d4f35f161344e3451df256abff9b0
Leonardo Holanda <leonardo_holanda1999@hotmail.com>
2022-09-17 14:43:41 -0300

    New project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+	public resultado: String = "Resultado"
+  public precoGasolina
+  public precoAlcool
+
+	calcular(){
+
+		//Validar se os campos foram preenchidos
+		if( this.precoAlcool && this.precoGasolina ){
+			
+			var pAlcool = parseFloat(this.precoAlcool)
+			var pGasolina = parseFloat(this.precoGasolina)
+
+			/*
+			Faz cÃ¡lculo (precoAlcool / precoGasolina)
+			se: resultado >= 0.7 melhor utilizar gasolina
+			senÃ£o: melhor utilizar Ã¡lcool
+			*/
+			var res = pAlcool / pGasolina
+			if( res >= 0.7){
+				this.resultado = "Melhor utilizar Gasolina"
+			}else{
+				this.resultado = "Melhor utilizar Ãlcool"
+			}
+
+		}else{
+			this.resultado = "Preencha corretamente os campos!"
+		}
+
+	}
 
 }
